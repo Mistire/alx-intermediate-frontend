578fd71e84211ed3b925e2c53917e2f00d6b2593
Mistire <mdiwithgod@gmail.com>
2025-09-07 22:52:32 +0300

    Building a Complex Page Layout with Nested CSS Grids Using Tailwind CSS

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./*.html",          
+    "./src/**/*.{js,html}" 
+  ],
   theme: {
     extend: {},
   },
